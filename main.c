228d4c20a061d383acd7537936a32523e8cda425
Carlos <velasquezcgm@gmail.com>
2018-04-19 02:28:02 -0300

    asd

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: 19 abr. 2018
+ *      Author: utnso
+ */
+#include <stdio.h> // Por dependencia de readline en algunas distros de linux :)
+
+
+int main() {
+	printf("hola");
+	return 0;
+}
